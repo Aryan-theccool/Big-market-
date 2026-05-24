dbfde468b9566fa7653f66e36f0318e577fdd7f7
Aryan-theccool <aryanbhadoria100@gmail.com>
2026-05-24 15:49:52 +0530

    feat: implement high-performance canvas drawing system with freehand support and Yjs synchronization infrastructure

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['nanoid'],
+  webpack: (config) => {
+    config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
